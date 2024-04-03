29350a9398009b8b56f8cb183e874c8a82abcc2a
Andrzej Liszka <andrzej.liszka@gmail.com>
2024-04-03 17:54:05 +0200

    wip19

--- usecases/offload/backupper.go
+++ usecases/offload/backupper.go
@@ -103,7 +103,7 @@ func (b *backupper) OnStatus(ctx context.Context, req *StatusRequest) (reqStat, 
 
 	meta, err := store.Meta(ctx)
 	if err != nil {
-		path := fmt.Sprintf("%s/%s", req.ID, OffloadFile)
+		path := fmt.Sprintf("%s/%s", req.ID, OffloadNodeFile)
 		return reqStat{}, fmt.Errorf("cannot get status while backing up: %w: %q: %v", errMetaNotFound, path, err)
 	}
 	if err != nil || meta.Error != "" {
